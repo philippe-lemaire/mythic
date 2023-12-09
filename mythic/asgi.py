312a7a7895088bc479d46d3ed7c9615cb4c476ff
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-12-09 12:11:03 +0100

    initial commit

---
+++ mythic/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mythic project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mythic.settings")
+
+application = get_asgi_application()
