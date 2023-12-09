92a8abf937635df965e92454496edd11caccc2e0
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-12-09 12:41:46 +0100

    form ok

---
+++ gmemulator/forms.py
@@ -0,0 +1,31 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django import forms
+
+
+class FateQuestionForm(forms.Form):
+    odd_labels = (
+        (1, "Certain"),
+        (2, "Nearly Certain"),
+        (3, "Very Likely"),
+        (4, "Likely"),
+        (5, "50/50"),
+        (6, "Unlikely"),
+        (7, "Very Unlikely"),
+        (8, "Nearly Impossible"),
+        (9, "Impossible"),
+    )
+
+    odds = forms.ChoiceField(
+        label="What are the odds?", choices=odd_labels, required=True, initial=5
+    )
+    chaos_factor = forms.IntegerField(
+        label="Chaos Factor", min_value=1, max_value=9, initial=1
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.form_action = "gmemulator:fate_question"
+        self.helper.add_input(Submit("submit", "Submit"))
--- gmemulator/views.py
+++ gmemulator/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .forms import FateQuestionForm
 
 # Create your views here.
 
 
 def fate_question(request):
-    context = {"question": "what time is it?"}
+    form = FateQuestionForm(request.POST or None)
+    context = {"form": form}
+    if request.method == "POST":
+        if form.is_valid():
+            # do the game logic
+            pass
+
     return render(request, "gmemulator/fate_question.html", context)
