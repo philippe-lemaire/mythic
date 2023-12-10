df5c44d62f654eece8589bcd41e8f5f9eaaa7fff
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-12-10 09:35:45 +0100

    added random event logic to the fate question

--- gmemulator/views.py
+++ gmemulator/views.py
@@ -6,17 +6,16 @@ from .roll import roll_d100
 
 def fate_question(request):
     form = FateQuestionForm(request.POST or None)
-    context = {"form": form}
+    context = {"form": form, "event": ""}
     if request.method == "POST":
         if form.is_valid():
             # do the game logic
             odd = form.cleaned_data["odds"]
-            print(odd)
-            print(type(odd))
-            print(fate_chart)
-            chaos_factor = int(form.cleaned_data["chaos_factor"]) - 1
-            mini, threshold, maxi = fate_chart.get(odd)[chaos_factor]
+            chaos_factor = int(form.cleaned_data["chaos_factor"])
+            mini, threshold, maxi = fate_chart.get(odd)[chaos_factor - 1]
             dice = roll_d100()
+            # just for testing
+
             if dice <= mini:
                 answer = "Exceptional Yes!"
             elif dice <= threshold:
@@ -25,6 +24,10 @@ def fate_question(request):
                 answer = "No"
             else:
                 answer = "Exceptional No!"
+            # check for event
+
+            if dice > 10 and len(set(str(dice))) == 1 and dice % 10 <= chaos_factor:
+                context["event"] = "Random Event"
             context["roll"] = dice
             context["answer"] = answer
 
